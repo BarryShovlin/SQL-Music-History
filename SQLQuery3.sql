3eaa8d71b3ae35d9af262016a5a4a4c14fa221ba
Barry Shovlin <bshovlin32@gmail.com>
2021-03-10 10:13:28 -0600

    initial commit

---
+++ SQLQuery3.sql
@@ -0,0 +1,19 @@
+﻿--Select	DISTINCT	--a.ArtistName--FROM	Album--JOIN	Artist a on ArtistId = a.id--JOIN	Genre g on GenreId = g.id--WHERE	g.id = 7;
+
+--SELECT
+--a.ArtistName
+--FROM Album
+--JOIN Artist a on ArtistId = a.id
+--JOIN Genre g on GenreId = g.id
+--WHERE g.id = 4 OR g.id = 2;
+
+--SELECT Title
+--FROM ALBUM
+--WHERE AlbumLength IS NULL
+
+
+
+--SELECT a.Title, s.Title FROM Album a LEFT JOIN Song s ON s.AlbumId = a.Id;
+--SELECT a.Title, s.Title FROM Song s LEFT JOIN Album a ON s.AlbumId = a.Id;
+SELECT a.Title, s.Title FROM Album a LEFT JOIN Song s ON s.AlbumId = a.Id
+
---
+++ SQLQuery4.sql
@@ -0,0 +1,3 @@
+﻿INSERT INTO Song (Title, SongLength, ReleaseDate, GenreId, ArtistId, AlbumId) VALUES ('Carousel', 851, '2/17/1995', 7, 33, 23);
+INSERT INTO Album (Title, ReleaseDate, AlbumLength, Label, ArtistId, GenreId) VALUES ('Cheshire Cat', '02/17/1995', 2268, 'Cargo Music', 33, 9);
+INSERT INTO Artist (ArtistName, YearEstablished) VALUES ('Blink 182', 1992);
